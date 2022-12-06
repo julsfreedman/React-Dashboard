7a4e67ab17b3ac10da990a6cc6178f852bd543f3
Julia Freedman <113859369+julsfreedman@users.noreply.github.com>
2022-12-06 02:11:17 -0500

    First Commit - SetUp

---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+function Square() {
+    return (
+        <div>
+            <h4>square</h4>
+        </div>
+    )
+};
+
+export default Square;
+
+//below is the same thing as above:
+// export default function Square() {
+//     return (
+//         <div>
+//             <h4>square</h4>
+//         </div>
+//     )
+// }
